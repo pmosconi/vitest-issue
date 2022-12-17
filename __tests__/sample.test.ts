618e71434cbd9454f11f8c917b3c690239d77e6f
Paolo Mosconi <pmosconi@gmail.com>
2022-12-17 19:24:37 +0100

    issue when spying on a function that is passed as parameter

--- __tests__/sample.test.ts
+++ __tests__/sample.test.ts
@@ -1,6 +1,28 @@
-import { assert, expect, test } from 'vitest';
-import { mySqrt } from '../src/index.mjs';
+import { afterEach, beforeAll, expect, test, SpyInstance, vi } from 'vitest';
+import { Sample } from '../src/index.mjs';
 
-test('index.mts should be included in coverage', () => {
-  expect(mySqrt(4)).toBe(2);
+let spy: SpyInstance;
+let client: Sample;
+
+beforeAll(async () => {
+  client = new Sample();
+  spy = vi
+    .spyOn(client, 'mySqrt')
+    .mockImplementation((_) => {
+      return 42;
+      });
+});
+
+test('call mySqrt', () => {
+  expect(client.mySqrt(4)).toBe(42);
+  expect(spy).toHaveBeenCalledTimes(1);
+});
+
+test('call withFn', () => {
+  expect(client.withFn(4)).toBe(42);
+  expect(spy).toHaveBeenCalledTimes(1);
+});
+
+afterEach(() => {
+  vi.resetAllMocks();
 });
--- src/index.mts
+++ src/index.mts
@@ -1,1 +1,14 @@
-export const mySqrt = (n: number): number => Math.sqrt(n);
+export class Sample {
+  constructor() {
+    console.log('Sample constructor');
+  }
+
+  mySqrt(n: number) {
+    return Math.sqrt(n);
+  }
+
+  withFn(n: number) {
+    const fn = () => this.mySqrt(n);
+    return fn();
+  }
+}
